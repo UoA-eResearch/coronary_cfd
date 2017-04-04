3fbae45f00d5e470daa24d8b35578bb1e37a1e74
Nick Young <nyou045@UoA.auckland.ac.nz>
2017-04-04 17:28:21 +1200

    init

---
+++ Assets/ParticleGenerator.cs
@@ -0,0 +1,76 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using System;
+
+public class ParticleGenerator : MonoBehaviour
+{
+	public GameObject particleSystemPrefab;
+	public GameObject arrowPrefab;
+	string[] separator = new string[] { ", " };
+
+	// Use this for initialization
+	void Start()
+	{
+		var sw = new System.Diagnostics.Stopwatch();
+		sw.Start();
+		Debug.Log("reading csv");
+		var csvObject = Resources.Load("export-new") as TextAsset;
+		var csv = csvObject.text.Split('\n');
+		var headers = csv[5].Split(separator, StringSplitOptions.RemoveEmptyEntries);
+		Vector3 modelOrigin = Vector3.zero;
+		var count = 0;
+		for (int i = 6; i < csv.Length; i+=50)
+		{
+			count++;
+			var line = csv[i];
+			if (line.Length == 0) continue;
+			var bits = line.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+			var d = new Dictionary<string, float>();
+			for (var j = 0; j < headers.Length; j++)
+			{
+				try
+				{
+					d[headers[j].Trim()] = float.Parse(bits[j]);
+				} catch (FormatException)
+				{
+					
+				}
+			}
+			var pos = new Vector3(d["X [ m ]"], d["Y [ m ]"], d["Z [ m ]"]);
+			var direction = new Vector3(d["Velocity u [ m s^-1 ]"], d["Velocity v [ m s^-1 ]"], d["Velocity w [ m s^-1 ]"]);
+			
+			if (i == 6)
+			{
+				modelOrigin = pos;
+			}
+
+			// Arrow glyph
+			var glyph = Instantiate(arrowPrefab);
+			glyph.transform.parent = gameObject.transform;
+			glyph.transform.localPosition = pos - modelOrigin;
+			glyph.transform.LookAt(glyph.transform.TransformPoint(direction));
+			glyph.transform.localScale = Vector3.one * direction.magnitude;
+
+			// Particle system
+			/*
+			var p = Instantiate(particleSystemPrefab);
+			p.transform.parent = gameObject.transform;
+			p.transform.localPosition = pos;
+			p.transform.LookAt(p.transform.TransformPoint(direction));
+			var ps = p.GetComponent<ParticleSystem>();
+			var ma = ps.main;
+			ma.startSpeed = direction.magnitude * 10;
+			*/
+		}
+		Debug.Log("plotted " + count + " data points in  " + sw.Elapsed);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
---
+++ Assets/VectorTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.LookAt(Vector3.one);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
